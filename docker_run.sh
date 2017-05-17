1b019c021043ee421a5977941b4a7be82221ee83
Jakub Blaszczyk | hybris <jakub.blaszczyk@hybris.com>
2017-05-17 17:23:48 +0200

    Fix docker run

--- docker_run.sh
+++ docker_run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -x
-docker run -ti --rm -v $(pwd)/input:/input -v $(pwd)/output:/output cpp_compiler "${1:-example}"
+docker run -t --rm -v $(pwd)/input:/input -v $(pwd)/output:/output cpp_compiler "${1:-example}"
